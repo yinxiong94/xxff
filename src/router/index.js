2e1f7abeaf80627555682679fa7b69873c6a00c8
hongliping123 <49940450+hongliping123@users.noreply.github.com>
2019-07-17 17:28:04 +0800

    修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,51 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Application from '../components/Application/Application'
-import release from '../components/release/release'
-import pirce from '../components/pirce/pirce'
-import withdrawal from '../components/withdrawal/withdrawal'
+import Vue from "vue";
+import Router from "vue-router";
+import Application from "../components/Application/Application";
+import release from "../components/release/release";
+import pirce from "../components/pirce/pirce";
+import withdrawal from "../components/withdrawal/withdrawal";
+import Login from "../components/Login";
+import register from "../components/register"
+import Home from "../components/Home"
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/Application',
-      name: 'Application',
+      path: "/",
+      name: "Home",
+      component: Home
+    },
+    {
+      path: "/Login",
+      name: "Login",
+      component: Login
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: register
+    },
+    {
+      path: "/Application",
+      name: "Application",
       component: Application
     },
     {
-      path: '/release',
-      name: 'release',
+      path: "/release",
+      name: "release",
       component: release
     },
     {
-      path: '/pirce',
-      name: 'pirce',
+      path: "/pirce",
+      name: "pirce",
       component: pirce
     },
     {
-      path: '/withdrawal',
-      name: 'withdrawal',
+      path: "/withdrawal",
+      name: "withdrawal",
       component: withdrawal
     }
   ]
-})
+});
