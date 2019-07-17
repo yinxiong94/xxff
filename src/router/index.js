971f148a0a194f5467287f045e221dc5911fe5ca
longen0707 <1393726657@qq.com>
2019-07-17 17:04:57 +0800

    Revert "发布应用"
    This reverts commit 4f348629e24939aa27a13b6e3bebcdbf78848de1.

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,27 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Application from '../components/Application/Application'
-import release from '../components/release/release'
-import pirce from '../components/pirce/pirce'
-import withdrawal from '../components/withdrawal/withdrawal'
+import Vue from "vue";
+import Router from "vue-router";
+import Login from "../components/Login";
+import register from "../components/register"
+import Home from "../components/Home"
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/Application',
-      name: 'Application',
-      component: Application
+      path: "/",
+      name: "Home",
+      component: Home
     },
     {
-      path: '/release',
-      name: 'release',
-      component: release
+      path: "/Login",
+      name: "Login",
+      component: Login
     },
     {
-      path: '/pirce',
-      name: 'pirce',
-      component: pirce
-    },
-    {
-      path: '/withdrawal',
-      name: 'withdrawal',
-      component: withdrawal
+      path:"/register",
+      name:"register",
+      component:register
     }
   ]
-})
+});
