2d5ec45bcf151a7f6c8a8e093b4bbab1a0f21a3a
hongliping123 <49940450+hongliping123@users.noreply.github.com>
2019-08-05 17:56:45 +0800

    修改上传

--- src/components/text.vue
+++ src/components/text.vue
@@ -1,34 +1,38 @@
 <template>
   <div>
-    <el-upload action="https://jsonplaceholder.typicode.com/posts/"
-               list-type="picture-card"
-               :on-preview="handlePictureCardPreview"
-               :on-remove="handleRemove">
-      <i class="el-icon-plus"></i>
+    <el-upload
+      class="upload-demo"
+      drag
+      name="file"
+      action
+      accept="application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+      :before-upload="uploadFile"
+      multiple
+    >
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text clearfix">
+        <span class="fl">将文件拖拽至此区域上传</span>
+        <em class="fl">点击上传</em>
+      </div>
     </el-upload>
-    <el-dialog :visible.sync="dialogVisible">
-      <img width="100%"
-           :src="dialogImageUrl"
-           alt />
-    </el-dialog>
   </div>
 </template>
 
 
 <script>
 export default {
-  data () {
-    return {
-      dialogImageUrl: "",
-      dialogVisible: false
-    };
+  data() {
+    return {};
   },
   methods: {
-    handleRemove (file, fileList) {
-    },
-    handlePictureCardPreview (file) {
-      this.dialogImageUrl = file.url;
-      this.dialogVisible = true;
+    uploadFile(file) {
+      var formData = new FormData();
+      formData.append("file", file);
+      var file = formData;
+      this.axios.post("FileUpLoad.ashx", file).then(res => {
+            console.log(res)
+      });
+      return false;
     }
   }
 };
